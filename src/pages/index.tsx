4fe0379d80c8add32608d545c7cebb62821d7e8e
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-12 07:13:23 -0300

    Suport Sass Suport

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1>Hello Worldd</h1>
+    <h1 className={ styles.title }>
+      Hello <span>World</span>
+      </h1>
   )
 }
 
